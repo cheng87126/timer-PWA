8df95a30e31561fcd919b5a7bd6b1eb3383e9fb0
cheng87126 <cheng87126@gmail.com>
2018-01-19 00:19:25 +0800

    update

--- sw.js
+++ sw.js
@@ -1,40 +1,41 @@
-this.addEventListener('install', function (event) {
+var ver = 'v1'
+self.addEventListener('install', function (event) {
 	event.waitUntil(
-		caches.open('v2').then(function (cache) {
+		caches.open(ver).then(function (cache) {
 			return cache.addAll([
-				'/',
-				'/css/app.css',
-				'/images/'
+				'./index.html',
+				'./css/app.css',
+				'./images/'
 			])
 		})
 	)
 })
 
-this.addEventListener('fetch', function (event) {
+self.addEventListener('fetch', function (event) {
 	event.respondWith(
 		caches.match(event.request).catch(function () {
 			return fetch(event.request).then(function (response) {
-				return caches.open('v2').then(function (cache) {
+				return caches.open(ver).then(function (cache) {
 					cache.put(event.request, response.clone())
 					return response
 				});
 			});
 		}).catch(function () {
-			return caches.match('/')
+			return caches.match('./index.html')
 		})
 	)
 })
 
-// this.addEventListener('activate', function (event) {
-// 	var cacheWhitelist = ['v2']
+self.addEventListener('activate', function (event) {
+	var cacheWhitelist = [ver]
 
-// 	event.waitUntil(
-// 		caches.keys().then(function (keyList) {
-// 			return Promise.all(keyList.map(function (key) {
-// 				if (cacheWhitelist.indexOf(key) === -1) {
-// 					return caches.delete(key)
-// 				}
-// 			}))
-// 		})
-// 	)
-// })
+	event.waitUntil(
+		caches.keys().then(function (keyList) {
+			return Promise.all(keyList.map(function (key) {
+				if (cacheWhitelist.indexOf(key) === -1) {
+					return caches.delete(key)
+				}
+			}))
+		})
+	)
+})
