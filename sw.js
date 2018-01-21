c705a9e9569bde07fa57a164f021ab29fca6441b
cheng87126 <cheng87126@gmail.com>
2018-01-21 17:08:25 +0800

    update

--- sw.js
+++ sw.js
@@ -1,7 +1,8 @@
-var ver = 'v1'
+var ver = 'sw-timer-v2'
 self.addEventListener('install', function (event) {
 	event.waitUntil(
 		caches.open(ver).then(function (cache) {
+			// cache.addAll()
 			return cache.addAll([
 				'./index.html',
 				'./css/app.css',
@@ -13,13 +14,17 @@ self.addEventListener('install', function (event) {
 
 self.addEventListener('fetch', function (event) {
 	event.respondWith(
-		caches.match(event.request).catch(function () {
-			return fetch(event.request).then(function (response) {
-				return caches.open(ver).then(function (cache) {
-					cache.put(event.request, response.clone())
-					return response
-				});
-			});
+		caches.match(event.request).then(function (res) {
+			if(res){
+				return res
+			}else{
+				return fetch(event.request).then(function (response) {
+					return caches.open(ver).then(function (cache) {
+						cache.put(event.request, response.clone())
+						return response
+					})
+				})
+			}
 		}).catch(function () {
 			return caches.match('./index.html')
 		})
