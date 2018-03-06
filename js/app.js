96a0d61f49846986912d3c4d5a46e26402f35b7e
cheng87126 <cheng87126@gmail.com>
2018-03-06 23:12:48 +0800

    update timer picker

--- js/app.js
+++ js/app.js
@@ -48,4 +48,51 @@
 	function fillZero(num){
 		return num.toString().replace(/^(\d)$/,'0$1')
 	}
+
+	function createTimeItem(){
+		var item = [],
+			itemDis = [],
+			tmp = [],
+			tmpDis = []
+		for(var h=0;h<24;h++){
+			tmp.push(h)
+			tmpDis.push(fillZero(h))
+		}
+		item.push(tmp)
+		itemDis.push(tmpDis)
+
+		tmp = []
+		tmpDis = []
+		for(var m=0;m<60;m++){
+			tmp.push(m)
+			tmpDis.push(fillZero(m))
+		}
+		item.push(tmp)
+		itemDis.push(tmpDis)
+		item.push(tmp)
+		itemDis.push(tmpDis)
+
+		return {
+			item:item,
+			itemDis:itemDis
+		}
+	}
+	var timeItem = createTimeItem()
+	var p = new picker({
+		title:'请选择时间',
+		item:timeItem.item,
+		itemDis:timeItem.itemDis
+	})
+	p.on('cancel',function(val){
+		console.log(idx)
+	})
+	p.on('confirm',function(val){
+		console.log(val.idx)
+		var time = val.idx
+		document.getElementById('time').value = time[0] * 60 + (+time[1])
+		btn.click()
+	})
+	document.querySelector('.setting').addEventListener('click',function(){
+		p.show()
+	},false)
 })()
